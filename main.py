ccabf6aaf0d8c31451c758bfe49b0f3f31276497
Vlad Saif <vlad9931@gmail.com>
2017-07-15 18:43:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,17 @@ def main():
     # build_word_frequencies_table()
     # build_stat_by_week()
     # build_stat_by_day()
-
-    day_plot(1)
-    day_accum_plot(1)
+    dialog_id = 1 # TODO: your own dialog
+    day_plot(dialog_id)
+    day_accum_plot(dialog_id)
+    
+    build_themes_relatively()
+    
+    with sqlite3.connect("themes.sqlite") as table:
+        cursor = table.cursor()
+        cursor.execute("SELECT * FROM rel_t%s" % dialog_id)
+        ans = cursor.fetchall()
+        pprint(sorted(ans, key=operator.itemgetter(1), reverse=True))
 
 
 if __name__ == '__main__':

829927afb23669022ab406d0bb4e8b88b3480868
Vlad Saif <vladsayf@gamil.com>
2017-07-15 18:39:11 +0500

    Refactoring

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from vkanalyzer.drawers import *
 
 def main():
     # TODO: use instruments from here
-    # token, user_id = vkanalyzer.vk.authorize.authorization()
-    # create_or_complete_database(token)
+    token, user_id = vkanalyzer.vk.authorize.authorization()
+    create_or_complete_database(token)
     # build_word_frequencies_table()
     # build_stat_by_week()
     # build_stat_by_day()
-    day_plot(10278660)
-    day_accum_plot(10278660)
+
+    day_plot(1)
+    day_accum_plot(1)
 
 
 if __name__ == '__main__':
